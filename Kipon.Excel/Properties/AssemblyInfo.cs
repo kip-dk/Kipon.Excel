2ada00669da024aca8aeaf821d791872f0bbda6f
Kjeld Poulsen <kip@kipon.dk>
2019-08-11 09:28:00 +0200

    license file added

--- Kipon.Excel/Properties/AssemblyInfo.cs
+++ Kipon.Excel/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Kipon.Excel")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library for streaming .NET POCO into excel and back (OpenXml)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Kipon ApS")]
 [assembly: AssemblyProduct("Kipon.Excel")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
