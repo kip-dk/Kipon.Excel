b18457817e8189ee9cca6584716e8d28cff3fb11
Kjeld Poulsen <kip@kipon.dk>
2019-08-11 08:29:39 +0200

    added snk

--- Kipon.Excel/WriterImplementation/UnittestVisability.cs
+++ Kipon.Excel/WriterImplementation/UnittestVisability.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleToAttribute("Kipon.Excel.UnitTests")]
+[assembly: InternalsVisibleToAttribute("Kipon.Excel.UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001002d02848b80ae044ccc7cbe4ba1ad53b1a7a9201a1778eec855fd8409ee22365637008ba7e3cade35e0a9427c78d0c21c48d7321f1b2becb9ea7934d62a6f6b0c33be50fb607bf7f709c51b53d5779073c48a0c5842521f946398b35746e8fe6b47a07e969979cddfb39185f091e058a4bf36a2fe5cecd3fa0cc2f675490f30a6")]
 namespace Kipon.Excel.WriterImplementation
 {
 }
